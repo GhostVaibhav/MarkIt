20bb46093ce3c99baf07a1d7262383a87a7fa418
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2021-05-25 17:53:11 +0530

    Modified "clean" file

--- clean.cpp
+++ clean.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <sys/stat.h>
 using namespace std;
+inline bool exist (const std::string& name) {
+    struct stat buffer;   
+    return (stat (name.c_str(), &buffer) == 0); 
+}
 int main() {
     #ifdef _WIN32
     system("cls");
@@ -11,8 +16,10 @@ int main() {
     system("del *.dat >nul 2>&1");
     system("del *.out >nul 2>&1");
     system("del test_debug >nul 2>&1");
+    system("del build.exe >nul 2>&1");
     system("del test >nul 2>&1");
     system("del generates >nul 2>&1");
+    system("del clean.exe >nul 2>&1");
     #else
     system("clear");
     system("rm *.exe > /dev/null 2>&1");
@@ -24,11 +31,23 @@ int main() {
     system("rm *.dat > /dev/null 2>&1");
     system("rm test_debug > /dev/null 2>&1");
     system("rm test > /dev/null 2>&1");
+    system("rm build > /dev/null 2>&1");
     system("rm generates > /dev/null 2>&1");
+    system("rm clean > /dev/null 2>&1");
     #endif
-    cout << "\u001b[32m+++++++++++++++++++++++++++++++++++++++++++++\u001b[0m\n";
-    cout << "\u001b[32m+               Clean Success               +\u001b[0m\n";
-    cout << "\u001b[32m+ Now remove the clean application manually +\u001b[0m\n";
-    cout << "\u001b[32m+++++++++++++++++++++++++++++++++++++++++++++\u001b[0m\n";
+    if(!exist("bin/test.exe") && !exist("bin/test_debug.exe") && !exist("test") && !exist("test_debug") &&! exist("test.o") && !exist("test.out") && !exist("test_debug.out") && !exist("generates") && !exist("clean") && !exist("clean.exe") && !exist("build") && !exist("build.exe") && !exist("data.dat") && !exist("bin/data.dat")) {
+        cout << "\u001b[32m   ________                          __\u001b[0m\n";
+        cout << "\u001b[32m  / ____/ /__  ____ _____  ___  ____/ /\u001b[0m\n";
+        cout << "\u001b[32m / /   / / _ \\/ __ `/ __ \\/ _ \\/ __  /\u001b[0m\n";
+        cout << "\u001b[32m/ /___/ /  __/ /_/ / / / /  __/ /_/ /\u001b[0m\n";
+        cout << "\u001b[32m\\____/_/\\___/\\__,_/_/ /_/\\___/\\__,_/\u001b[0m\n\n";
+    }
+    else {
+        cout << "\u001b[31m    _   __      __     ________                          __\u001b[0m\n";
+        cout << "\u001b[31m   / | / /___  / /_   / ____/ /__  ____ _____  ___  ____/ /\u001b[0m\n";
+        cout << "\u001b[31m  /  |/ / __ \\/ __/  / /   / / _ \\/ __ `/ __ \\/ _ \\/ __  /\u001b[0m\n";
+        cout << "\u001b[31m / /|  / /_/ / /_   / /___/ /  __/ /_/ / / / /  __/ /_/ /\u001b[0m\n";
+        cout << "\u001b[31m/_/ |_/\\____/\\__/   \\____/_/\\___/\\__,_/_/ /_/\\___/\\__,_/\u001b[0m\n\n";
+    }
     return 0;
 }
