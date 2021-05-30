5704a69bc73ff168ae27734161591bdf524d70ad
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2021-05-30 23:47:35 +0530

    Modified "clean" file

--- clean.cpp
+++ clean.cpp
@@ -35,6 +35,23 @@ int main() {
     system("rm generates > /dev/null 2>&1");
     system("rm clean > /dev/null 2>&1");
     #endif
+    #ifdef _WIN32
+    if(!exist("bin/test.exe") && !exist("bin/test_debug.exe") && !exist("test") && !exist("test_debug") &&! exist("test.o") && !exist("test.out") && !exist("test_debug.out") && !exist("generates") && !exist("clean") && !exist("build") && !exist("build.exe") && !exist("data.dat") && !exist("bin/data.dat")) {
+        cout << "\u001b[32m   ________                          __\u001b[0m\n";
+        cout << "\u001b[32m  / ____/ /__  ____ _____  ___  ____/ /\u001b[0m\n";
+        cout << "\u001b[32m / /   / / _ \\/ __ `/ __ \\/ _ \\/ __  /\u001b[0m\n";
+        cout << "\u001b[32m/ /___/ /  __/ /_/ / / / /  __/ /_/ /\u001b[0m\n";
+        cout << "\u001b[32m\\____/_/\\___/\\__,_/_/ /_/\\___/\\__,_/\u001b[0m\n\n";
+        cout << "\u001b[32m      Remove clean.exe manually\u001b[0m\n";
+    }
+    else {
+        cout << "\u001b[31m    _   __      __     ________                          __\u001b[0m\n";
+        cout << "\u001b[31m   / | / /___  / /_   / ____/ /__  ____ _____  ___  ____/ /\u001b[0m\n";
+        cout << "\u001b[31m  /  |/ / __ \\/ __/  / /   / / _ \\/ __ `/ __ \\/ _ \\/ __  /\u001b[0m\n";
+        cout << "\u001b[31m / /|  / /_/ / /_   / /___/ /  __/ /_/ / / / /  __/ /_/ /\u001b[0m\n";
+        cout << "\u001b[31m/_/ |_/\\____/\\__/   \\____/_/\\___/\\__,_/_/ /_/\\___/\\__,_/\u001b[0m\n\n";
+    }
+    #else
     if(!exist("bin/test.exe") && !exist("bin/test_debug.exe") && !exist("test") && !exist("test_debug") &&! exist("test.o") && !exist("test.out") && !exist("test_debug.out") && !exist("generates") && !exist("clean") && !exist("clean.exe") && !exist("build") && !exist("build.exe") && !exist("data.dat") && !exist("bin/data.dat")) {
         cout << "\u001b[32m   ________                          __\u001b[0m\n";
         cout << "\u001b[32m  / ____/ /__  ____ _____  ___  ____/ /\u001b[0m\n";
@@ -49,5 +66,6 @@ int main() {
         cout << "\u001b[31m / /|  / /_/ / /_   / /___/ /  __/ /_/ / / / /  __/ /_/ /\u001b[0m\n";
         cout << "\u001b[31m/_/ |_/\\____/\\__/   \\____/_/\\___/\\__,_/_/ /_/\\___/\\__,_/\u001b[0m\n\n";
     }
+    #endif
     return 0;
 }
