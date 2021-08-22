f7edf51eef27e81d1d0359eb5f41e0d5a2b65512
GhostVaibhav <48472541+GhostVaibhav@users.noreply.github.com>
2021-08-22 23:53:36 +0530

    Modified the "build" file for compiling "tabulate"; changed "c++17" to "gnu++17"

--- build.cpp
+++ build.cpp
@@ -15,7 +15,7 @@ int main(int argc,char* argv[]) {
             system("clear");
         #endif
         cout << "- Compiling debug object file\n";
-        cmd = "g++ -Iinclude -Og -g -w -c -std=c++17 " + fileName + ".cpp";
+        cmd = "g++ -Iinclude -Og -g -w -c -std=gnu++17 " + fileName + ".cpp";
         system(cmd.c_str());
         if(!exist("main.o")) {
             #ifdef __APPLE__
@@ -70,7 +70,7 @@ int main(int argc,char* argv[]) {
         cout << "\u001b[32m- Build Success - Debug\u001b[0m\n";
         #endif
         cout << "- Compiling release object file\n";
-        cmd = "g++ -Iinclude -Ofast -w -c -s -std=c++17 " + fileName + ".cpp";
+        cmd = "g++ -Iinclude -Ofast -w -c -s -std=gnu++17 " + fileName + ".cpp";
         system(cmd.c_str());
         if(!exist("main.o")) {
             #ifdef __APPLE__
